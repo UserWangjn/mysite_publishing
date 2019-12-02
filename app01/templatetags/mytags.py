ec7e4c9cf1568a5c635742bdd30edc7b0aa9ca5d
wjn <wjntest@163.com>
2019-12-02 23:57:05 +0800

    add inclusion_tag

---
+++ app01/templatetags/mytags.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+# @Author: wjn
+
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('pagination.html')
+def pagination(total,current_num):
+    return {'total':range(1,total+1),'current_num':current_num}
