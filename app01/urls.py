f262f79238beccd01ddf9c7e90235a222bdbb481
wjn <wjntest@163.com>
2019-12-12 15:48:38 +0800

    增加内容

--- app01/urls.py
+++ app01/urls.py
@@ -9,14 +9,15 @@ urlpatterns = [
     # 在django1.1中用url，在django2.0中用path或re_path，其中re_path的用法和1.1中的url是一样的
     re_path(r'^add_publishing/$', views.AddPublishing.as_view(),name='add_publishing'),
     re_path(r'^edit_publishing/', views.edit_publishing),
-    url(r'^delete_publishing/(?P<delete_id>\d+)$', views.delete_publishing,name='delete_publishing'),  # (?P<delete_id>\d+)命名分组
+    url(r'delete_(?P<table>publishing|book|author)/(?P<delete_id>\d+)$',views.delete,name='delete'),
+    # url(r'^delete_publishing/(?P<delete_id>\d+)$', views.delete_publishing,name='delete_publishing'),  # (?P<delete_id>\d+)命名分组
     # url(r'^delete_publishing/(\d+)$', views.delete_publishing),  # # (\d+)分组
-    url(r'^book_list/$',views.book_list),
+    url(r'^book/$',views.book_list,name='book_list'),
     url(r'^add_book/$',views.add_book),
     url(r'^edit_book/$',views.edit_book),
-    url(r'^delete_book/$',views.delete_book),
-    url(r'^author_list/$',views.author_list),
+    # url(r'^delete_book/$',views.delete_book),
+    url(r'^author_list/$',views.author_list,name='author_list'),
     url(r'^add_author/$',views.add_author),
-    url(r'^delete_author/$',views.delete_author),
+    # url(r'^delete_author/$',views.delete_author),
     url(r'^edit_author/$',views.edit_author),
 ]
--- app01/views.py
+++ app01/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, HttpResponse, redirect
 from app01.models import Publishing, Book,Author
 from django.views import View
 from django.urls import reverse
+from app01 import models
 
 
 # Create your views here.
 # 出版社列表展示
-def publishing_list(request):
+def publishing_list(request,*args,**kwargs):
     ret = Publishing.objects.all()
     # print(ret)
-
     return render(request, 'list/publishing_list.html', {'ret': ret})
 
 
@@ -75,7 +75,7 @@ def add_book(request):
         book_name = request.POST.get('book_name')
         new_publishing_id = request.POST.get('publishing_id')
         Book.objects.create(title=book_name, publishing_id=new_publishing_id)
-        return redirect('/book_list/')
+        return redirect(reverse('book_list'))
     return render(request, 'add_book2.html', {'publishing_list': publishing_list})
 
 
@@ -91,7 +91,7 @@ def edit_book(request):
         old_book_obj.title = new_book_title
         old_book_obj.publishing_id = new_publishin_id
         old_book_obj.save()
-        return redirect('/book_list/')
+        return redirect(reverse('book_list'))
     publishing_list_obj = Publishing.objects.all()
     return render(request,'edit_book2.html',{'publishing_list_obj':publishing_list_obj,'old_book_obj':old_book_obj})
 
@@ -122,14 +122,14 @@ def add_author(request):
         new_book_ids = request.POST.getlist('book_ids')
         new_author_obj = Author.objects.create(name=new_author_name)
         new_author_obj.books.add(*new_book_ids)
-        return redirect('/author_list/')
+        return redirect(reverse('author_list'))
     return render(request,'add_author.html',{'book_list_obj':book_list_obj})
 
 
 def delete_author(request):
     delete_author_id = request.GET.get('shanchu')
     Author.objects.get(id=delete_author_id).delete()
-    return redirect('/author_list/')
+    return redirect(reverse('author_list'))
 
 
 def edit_author(req):
@@ -142,7 +142,7 @@ def edit_author(req):
         old_author_obj.name = new_author_name
         old_author_obj.save()
         old_author_obj.books.set(new_book_ids)
-        return redirect('/author_list/')
+        return redirect(reverse('author_list'))
     return render(req,
                   'edit_author.html',
                   {'author_obj':old_author_obj,'book_list_obj':book_list_obj})
@@ -160,3 +160,12 @@ class AddPublishing(View):
         Publishing.objects.create(name=publishing_name)
         # return redirect('/publishing_list/')
         return redirect(reverse('publishing_list'))
+
+
+def delete(request,table,delete_id,*args,**kwargs):
+    # 1.获取要删除的内容：表名
+    table_class = getattr(models,table.capitalize())
+    # 2.去数据库删除该内容
+    table_class.objects.get(id=delete_id).delete()
+    # 3.返回list页面
+    return redirect(reverse('{}_list'.format(table)))
